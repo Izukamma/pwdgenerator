ffebbf8a2c646390e75adee4ede333fdab394e08
unknown <izukammagabriel@gmail.com>
2021-11-06 12:58:25 -0500

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+
+  let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+
+  for (let i = 0; i < 20; i++) {
+      let generate = letters[Math.floor(Math.random() * 64)];
+      password += generate;
+
+  }
+
+  document.getElementById("password").innerHTML = password;
+}
